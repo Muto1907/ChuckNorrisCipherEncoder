9b4f84011c28c95f6df5f9a9b7938e1fc1270522
Mahmut <Mahmut.Cinar93@outlook.de>
2025-08-13 23:17:52 +0200

    Implement encoding of Strings to Chuck Norris Cipher

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,40 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input string:");
+        String input = scanner.nextLine();
+        System.out.println("\nThe result:");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        StringBuilder binary = new StringBuilder();
+        for (char c : input.toCharArray()){
+            binary.append(String.format("%7s",
+                    Integer.toBinaryString(c)).replace(" ", "0"));
         }
+
+        System.out.println(encodeToChuckNorris(binary.toString()));
+    }
+    public static String encodeToChuckNorris(String binary){
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        while(i < binary.length()){
+            if(binary.charAt(i) == '0'){
+                result.append("00 0");
+            } else if (binary.charAt(i) == '1'){
+                result.append("0 0");
+            }
+            i++;
+            while (i < binary.length() && binary.charAt(i-1) == binary.charAt(i)){
+                result.append("0");
+                i++;
+            }
+            if (i < binary.length()){
+                result.append(" ");
+            }
+        }
+        return result.toString();
     }
 }
