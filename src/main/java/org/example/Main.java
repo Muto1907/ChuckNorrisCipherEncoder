f3051cd12e42f60e8d2a3d04f2afdb3660f12c23
Mahmut <Mahmut.Cinar93@outlook.de>
2025-08-17 20:28:55 +0200

    Implement Chuck Norris Cypher Decoder

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,18 +5,39 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Input string:");
+        System.out.println("Input encoded string:");
         String input = scanner.nextLine();
         System.out.println("\nThe result:");
-
-        StringBuilder binary = new StringBuilder();
-        for (char c : input.toCharArray()){
-            binary.append(String.format("%7s",
-                    Integer.toBinaryString(c)).replace(" ", "0"));
-        }
-
-        System.out.println(encodeToChuckNorris(binary.toString()));
+        String binary = decodeChuckNorris(input);
+        char[] binaryChars = decodeBinarytoASCII(binary);
+        System.out.println(String.valueOf(binaryChars));
     }
+
+    public static String decodeChuckNorris(String chuckNorris){
+        StringBuilder result = new StringBuilder();
+
+        String[] arr = chuckNorris.split(" ");
+
+        for (int i = 0; i < arr.length; i+= 2){
+            String literal;
+            if (arr[i].length() == 1) {
+                literal = "1";
+            } else {
+                literal = "0";
+            }
+            result.append(literal.repeat(arr[i+1].length()));
+        }
+        return result.toString();
+    }
+
+    public static char[] decodeBinarytoASCII(String binary){
+        char[] result = new char[binary.length() / 7];
+        for (int i = 0; i < binary.length(); i += 7){
+            result[i/7] = (char) Integer.parseInt(binary.substring(i, i + 7), 2);
+        }
+        return result;
+    }
+
     public static String encodeToChuckNorris(String binary){
         StringBuilder result = new StringBuilder();
         int i = 0;
